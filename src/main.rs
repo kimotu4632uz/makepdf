73e13c36c0a8588a37cd75b7695fee8febb43b20
kimotu4632uz <32472396+kimotu4632uz@users.noreply.github.com>
2022-12-19 22:10:17 +0900

    finish implementation

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,56 @@
 use std::io::{self, Write};
-use itertools::Itertools;
+
+use clap::{Parser, Subcommand};
 
 use makepdf::*;
 
-enum Operation {
-    Help,
+// definition of command line args
+#[derive(Parser)]
+#[clap(author, about, version)]
+/// make pdf from URL or images interactively.
+struct Arg {
+}
+
+
+// definition of interactive commands
+#[derive(Parser)]
+#[clap(name = "", disable_help_flag = true)]
+/// make pdf from URL or images interactively.
+struct OprArg {
+    #[clap(subcommand)]
+    cmd: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// exit command
     Quit,
+
+    /// backup files
     Backup,
+
+    /// trim pictures
     Trim,
+
+    /// generate pdf from pictures
     Genpdf,
-    Get(String),
+
+    /// get image from URL
+    Get { url: String },
 }
 
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    // parse command line args (for help and version flags)
+    Arg::parse();
+
+    // show help message of commands
+    // 1st element of argument is program name, so empty string
+    if let Err(e) = OprArg::try_parse_from(["", "help"].into_iter()) {
+        e.print()?;
+        println!();
+    }
 
     loop {
         {
@@ -24,32 +60,41 @@ async fn main() -> anyhow::Result<()> {
         }
 
         let mut line = String::new();
+        // return 0 if EOF
         if io::stdin().read_line(&mut line)? == 0 {
-            println!("");
+            println!();
             break;
         }
 
-        let mut cmds = line.split_whitespace();
-        let cmd = cmds.next();
-        let args = cmds.collect_vec();
+        // parse commands
+        // 1st element of argument must be program name, so add empty string
+        let cmds = [""].into_iter().chain(line.split_whitespace());
+        let args = match OprArg::try_parse_from(cmds) {
+            Ok(v) => v,
+            Err(e) => {
+                e.print()?;
+                println!();
+                continue
+            },
+        };
 
-//            match cmd {
-//                "help"   => help(),
-//                "quit"   => break,
-//                "backup" => backup().await?,
-//                "trim"   => trim().await?,
-//                "genpdf" => genpdf().await?,
-//                "get"    => {
-//                    if args.len() > 0 {
-//                        getimgs(args[0]).await?
-//                    } else {
-//                        println!("missing 1 argument for operation get")
-//                    }
-//                },
-//                other => {
-//                    println!("no such command: {}", other)
-//                },
-//            }
+        match args.cmd {
+            Commands::Quit => {
+                break;
+            },
+            Commands::Backup => {
+                backup().await?;
+            },
+            Commands::Trim => {
+                trim().await?;
+            },
+            Commands::Genpdf => {
+                genpdf().await?;
+            },
+            Commands::Get { url } => {
+                getimgs(&url).await?;
+            },
+        }
     }
 
     Ok(())
