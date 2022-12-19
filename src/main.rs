0b37ab3f2a61abdb9b40d75664822b702851857c
kimotu4632uz <32472396+kimotu4632uz@users.noreply.github.com>
2022-12-19 19:45:05 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,55 @@
-use std::io;
+use std::io::{self, Write};
+use itertools::Itertools;
 
-#[derive(Debug)]
+use makepdf::*;
+
 enum Operation {
-    Get,
-    GenPdf
+    Help,
+    Quit,
+    Backup,
+    Trim,
+    Genpdf,
+    Get(String),
 }
+
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    loop {
-        let mut line = String::new();
-        io::stdin().read_line(&mut line)?;
 
-        break;
+    loop {
+        {
+            let mut stdout = io::stdout().lock();
+            stdout.write_all(b"> ")?;
+            stdout.flush()?;
+        }
+
+        let mut line = String::new();
+        if io::stdin().read_line(&mut line)? == 0 {
+            println!("");
+            break;
+        }
+
+        let mut cmds = line.split_whitespace();
+        let cmd = cmds.next();
+        let args = cmds.collect_vec();
+
+//            match cmd {
+//                "help"   => help(),
+//                "quit"   => break,
+//                "backup" => backup().await?,
+//                "trim"   => trim().await?,
+//                "genpdf" => genpdf().await?,
+//                "get"    => {
+//                    if args.len() > 0 {
+//                        getimgs(args[0]).await?
+//                    } else {
+//                        println!("missing 1 argument for operation get")
+//                    }
+//                },
+//                other => {
+//                    println!("no such command: {}", other)
+//                },
+//            }
     }
 
     Ok(())
